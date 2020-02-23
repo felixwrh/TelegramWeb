5b406054c6612438c23dd28348d02e8746622b5f
doubleristretto <doubleristretto@users.noreply.github.com>
2020-02-24 01:05:06 +0800

    skeleton code for quiz 2

---
+++ app/src/main/java/edu/singaporetech/busstopdb/MainActivity.kt
@@ -0,0 +1,21 @@
+package edu.singaporetech.busstopdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ *  ICT2105 Quiz 2
+ *  Some comments here?
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // TODO implement floating action button
+
+    }
+
+    // TODO need to create a separate classes for the relational database elsewhere? (not here!)
+}
